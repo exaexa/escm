8e32af7c53cea298dba28e5130ef4a8e44c4e914
Mirek Kratochvil <exa.exa@gmail.com>
2007-09-08 19:50:31 +0200

    local frames 1
    Signed-off-by: Mirek Kratochvil <exa.exa@gmail.com>

--- src/types.cpp
+++ src/types.cpp
@@ -123,19 +123,60 @@ scm* hashed_frame::define (scm_env*e, symbol*s, scm*d)
 {
 	int hash = hash_string ( (const char*) dataof (s->d) );
 	chained_frame_entry*f;
-	
+
 	if (lookup_frame (s, &f) ) {
 		f->content = d;
 		return s;
 	}
-	
+
 	f = new_scm (*e, chained_frame_entry, s, d,
 	             ( (chained_frame_entry**) dataof (table) ) [hash]);
 	if (!f) return 0;
 
 	( (chained_frame_entry**) dataof (table) ) [hash] = f;
-	
+
 	return s;
 }
 
+/*
+ * LOCAL FRAMES
+ */
 
+/*
+ * about:
+ * If we decide the frame is full, we'll chain it to parent to
+ * add another binding. Good thing is that we can preallocate some
+ * memory (so it runs better later), bad thing is that we should know how
+ * much of it shall we use. Maybe add some destructor-triggered statistic
+ * collector of "ooh how many free positions/overflows we had this time"
+ * and compute the value after it. Algorithmization is unknown yet:D
+ */
+
+local_frame::local_frame (scm_env*e, size_t s) : frame (e)
+{
+	table = new_data_scm (*e, 2 * sizeof (scm*) * s);
+	if (!table) return;
+	size = s;
+	used = 0;
+}
+
+bool local_frame::lookup (symbol*name, scm**result)
+{
+	int s = 0, e = size, t, i;
+	scm**p = (scm**) dataof (table);
+
+	while (s <= e) {
+		i = (s + e) / 2;
+		t = name->cmp ( (symbol*) p[i*2]);
+		if (!t) {
+			*result = p[i*2];
+			return true;
+		} else 	if (t < 0) {
+			e = i - 1;
+		} else {
+			s = i + 1;
+		}
+	}
+
+	return false;
+}
