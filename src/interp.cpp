934ea7c094dff8f3225e0fbec2133473c19c399d
Mirek Kratochvil <exa.exa@gmail.com>
2007-12-27 10:33:40 +0100

    read-eval-loop

--- src/interp.cpp
+++ src/interp.cpp
@@ -11,6 +11,16 @@ int run_interpreter (int argc, char**argv)
 	scm_env e;
 
 	escm_add_scheme_builtins (&e);
+	
+	char cmdbuffer[513];
+	int n;
+	while(1){
+		n=fread(cmdbuffer,1,512,stdin);
+		cmdbuffer[n]=0;
+		e.eval_string(cmdbuffer);
+		e.run_eval_loop();
+		printf("=> %p\n",e.get_last_result());
+	}
 
 	return 0;
 }
